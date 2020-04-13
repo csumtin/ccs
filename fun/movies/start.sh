db3bf367fc216549ac4e1c8aaf08b32fa9036b89
csumtin <csumtin@protonmail.com>
2020-04-13 16:34:00 -0400

    Start movies

--- fun/movies/start.sh
+++ fun/movies/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -eux
 cd "$(dirname "$0")"
-sudo systemd-nspawn -D bootstrap -u c -M movies --setenv=DISPLAY=$DISPLAY --bind-ro=/tmp/.X11-unix --bind-ro=$XDG_RUNTIME_DIR/pulse:/container$XDG_RUNTIME_DIR/pulse --setenv=PULSE_SERVER=/container/$XDG_RUNTIME_DIR/pulse/native
+sudo systemd-nspawn -D bootstrap -u c -M movies --setenv=DISPLAY=$DISPLAY --bind-ro=/tmp/.X11-unix --bind-ro=$XDG_RUNTIME_DIR/pulse:/container$XDG_RUNTIME_DIR/pulse --setenv=PULSE_SERVER=/container/$XDG_RUNTIME_DIR/pulse/native /bin/bash <<EOT
+cd
+./Popcorn-Time
+EOT
